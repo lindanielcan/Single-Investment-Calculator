ce98cdd796376e0f3b049217e00dc64c2ce57712
XCanL996 <lindanielcan_academic@hotmail.com>
2022-02-14 15:06:08 -0500

    Update main_screen_controller.py

--- controller/main_screen_controller.py
+++ controller/main_screen_controller.py
@@ -1,7 +1,18 @@
+from model import calculator
+from model import investment_data
+
+
 class MainScreenController:
     def __init__(self):
-        self.data = []
+        """This class receives data from the main screen, sends data to model files,
+        and receives results from calculator class."""
+        self.calculator = calculator.Calculator()
+        self.investment_data = investment_data.InvestmentData()
 
     def get_investment_data(self, data):
-        self.data = data
-        print(data)
+        """Send data."""
+        self.investment_data.parse_data(data)
+
+    def get_result(self):
+        """Gets data from the calculator class and sends it main screen."""
+        return self.calculator.calculate_investment_total_return()
