b8ed4ef18e9be9b5094ca45eedc35e704c4f3ddc
XCanL996 <lindanielcan_academic@hotmail.com>
2022-02-20 22:01:17 -0500

    Update result_screen.py

--- view/result_screen.py
+++ view/result_screen.py
@@ -2,11 +2,14 @@ from tkinter import Tk
 from tkinter import Toplevel
 from view import main_screen
 
+
 class ResultScreen():
     def __init__(self, Tk):
         self.main_screen = Tk
         pass
 
     def open(self):
-        top = Toplevel(self.main_screen, height=500, width=600, bg=main_screen.BACKGROUND_COLOR)
+        top = Toplevel(self.main_screen, height=500, width=600, bg=main_screen.BACKGROUND_COLOR, highlightthickness=0)
+        top.resizable(False, False)
+        top.title("Result")
         top.mainloop()
