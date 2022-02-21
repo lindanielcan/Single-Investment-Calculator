3c3054c48f5a4aef2812acde5533a13d53304de4
XCanL996 <86374458+XCanL996@users.noreply.github.com>
2022-02-21 10:06:08 -0500

    result screen controller created

---
+++ controller/result_screen_controller.py
@@ -0,0 +1,4 @@
+class ResultScreenController:
+    def __init__(self):
+        """This class is responsible for receiving data from the calculator, validating data, and sending data to the result screen."""
+        pass
